73437b31c7d9a72596d8d2c862a30e9de5f3c068
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-08-19 15:36:53 +0900

    add account_set_balance.c

---
+++ sandbox/account_set_balance.c
@@ -0,0 +1,23 @@
+#include "stdlib.h"
+
+struct account {
+    int balance;
+};
+typedef struct account account_t;
+
+void account_set_balance(account_t* my_account, int new_balance)
+{
+    my_account->balance = new_balance;
+}
+
+int main()
+    //@ requires true;
+    //@ ensures true;
+{
+    account_t* my_account = malloc(sizeof(account_t));
+    if (!my_account) abort();
+    account_set_balance(my_account, 5);
+    free(my_account);
+
+    return 0;
+}
