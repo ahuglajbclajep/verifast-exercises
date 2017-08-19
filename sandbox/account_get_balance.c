52c8c6915fc8be1d04fedaae1cb2fffad2fd878b
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-08-19 17:54:59 +0900

    add account_get_balance.c

---
+++ sandbox/account_get_balance.c
@@ -0,0 +1,51 @@
+#include "stdlib.h"
+
+struct account {
+    int balance;
+};
+typedef struct account account_t;
+
+account_t* create_account()
+    //@ requires true;
+    //@ ensures  result->balance |-> _ &*& malloc_block_account(result);
+{
+    account_t* tmp = malloc(sizeof(account_t));
+    if (!tmp) abort();
+    tmp->balance = 0;
+
+    return tmp;
+}
+
+int account_get_balance(account_t* my_account)
+    //@ requires my_account->balance |-> _;
+    //@ ensures  my_account->balance |-> _;
+{
+    return my_account->balance;
+}
+
+void account_set_balance(account_t* my_account, int new_balance)
+    //@ requires my_account->balance |-> _;
+    //@ ensures  my_account->balance |-> new_balance;
+{
+    my_account->balance = new_balance;
+}
+
+void dispose_account(account_t* my_account)
+    //@ requires my_account->balance |-> _ &*& malloc_block_account(my_account);
+    //@ ensures  true;
+{
+    free(my_account);
+}
+
+int main()
+    //@ requires true;
+    //@ ensures  true;
+{
+    account_t* my_account = create_account();
+    account_set_balance(my_account, 5);
+    int tmp = account_get_balance(my_account);
+    assert(tmp == 5);
+    dispose_account(my_account);
+
+    return 0;
+}
