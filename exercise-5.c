d3fb757f5729d3408dbda601f28f3e5a97832e8b
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-08-20 16:26:36 +0900

    verify ex5

--- exercise-5.c
+++ exercise-5.c
@@ -24,36 +24,55 @@ predicate stack(stack_t* stack, int size) =
 @*/
 
 stack_t* create_stack()
+    //@ requires true;
+    //@ ensures  stack(result, 0);
 {
     stack_t* stack = malloc(sizeof(stack_t));
     if (!stack) abort();
     stack->head = 0;
 
+    //@ close nodes(0, 0);
+    //@ close stack(stack, 0);
     return stack;
 }
 
 void stack_push(stack_t* stack, int value)
+    //@ requires stack(stack, ?size);
+    //@ ensures  stack(stack, size + 1);
 {
     node_t* new_node = malloc(sizeof(node_t));
     if (!new_node) abort();
     new_node->value = value;
+
+    //@ open stack(stack, size);
     new_node->next = stack->head;
     stack->head = new_node;
+    //@ close nodes(new_node, size + 1);
+    //@ close stack(stack, size + 1);
 }
 
 int stack_pop(stack_t* stack)
+    //@ requires stack(stack, ?size) &*& 0 < size;
+    //@ ensures  stack(stack, size -1);
 {
+    //@ open stack(stack, size);
     node_t* head = stack->head;
+    //@ open nodes(head, size);
     int value = head->value;
 
     stack->head = head->next;
     free(head);
+    //@ close stack(stack, size - 1);
 
     return value;
 }
 
 void stack_dispose(stack_t* stack)
+    //@ requires stack(stack, 0);
+    //@ ensures true;
 {
+    //@ open stack(stack, _);
+    //@ open nodes(_, _);
     free(stack);
 }
 
@@ -64,8 +83,8 @@ int main()
     stack_t* stack = create_stack();
     stack_push(stack, 10);
     stack_push(stack, 20);
-    assert(stack_pop(stack) == 20);
-    assert(stack_pop(stack) == 10);
+    stack_pop(stack);
+    stack_pop(stack);
     stack_dispose(stack);
 
     return 0;
